7dddbf396bd7b3f5d3d72cda81a1f0b12e1ef640
Jaco Greeff <jaco@parity.io>
2017-09-29 12:44:53 +0200

    Add Hardware confirmation component

---
+++ Signer/ConfirmViaHardware/confirmViaHardware.js
@@ -0,0 +1,115 @@
+// Copyright 2015-2017 Parity Technologies (UK) Ltd.
+// This file is part of Parity.
+
+// Parity is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity.  If not, see <http://www.gnu.org/licenses/>.
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import ReactTooltip from 'react-tooltip';
+
+import Button from '../../Button';
+import Form from '../../Form';
+import Input from '../../Form/Input';
+import IdentityIcon from '../../IdentityIcon';
+
+import styles from '../ConfirmViaPassword/confirmViaPassword.css';
+
+export default class ConfirmViaHardware extends Component {
+  static contextTypes = {
+    api: PropTypes.object.isRequired
+  };
+
+  static propTypes = {
+    account: PropTypes.object,
+    address: PropTypes.string.isRequired,
+    isDisabled: PropTypes.bool,
+    isFocussed: PropTypes.bool,
+    isSending: PropTypes.bool.isRequired,
+    onConfirm: PropTypes.func.isRequired
+  };
+
+  render () {
+    const { account, address, isDisabled, isSending, onConfirm } = this.props;
+
+    return (
+      <div className={ styles.confirmForm }>
+        <Form>
+          { this.renderHint() }
+          <div
+            data-effect='solid'
+            data-for={ `transactionConfirmForm${this.id}` }
+            data-place='bottom'
+            data-tip
+          >
+            <Button
+              className={ styles.confirmButton }
+              isDisabled={ isDisabled || isSending }
+              fullWidth
+              icon={
+                <IdentityIcon
+                  address={ address }
+                  button
+                  className={ styles.signerIcon }
+                />
+              }
+              label={
+                isSending
+                  ? (
+                    <FormattedMessage
+                      id='signer.txPendingConfirm.buttons.confirmBusy'
+                      defaultMessage='Confirming...'
+                    />
+                  )
+                  : (
+                    <FormattedMessage
+                      id='signer.txPendingConfirm.buttons.confirmRequest'
+                      defaultMessage='Confirm Request'
+                    />
+                  )
+              }
+              onClick={ onConfirm }
+            />
+          </div>
+        </Form>
+      </div>
+    );
+  }
+
+  renderHint () {
+    const { isDisabled, isSending } = this.props;
+
+    if (isSending) {
+      return (
+        <div className={ styles.passwordHint }>
+          <FormattedMessage
+            id='signer.sending.hardware.confirm'
+            defaultMessage='Please confirm the transaction on your attached hardware device'
+          />
+        </div>
+      );
+    } else if (isDisabled) {
+      return (
+        <div className={ styles.passwordHint }>
+          <FormattedMessage
+            id='signer.sending.hardware.connect'
+            defaultMessage='Please attach your hardware device before confirming the transaction'
+          />
+        </div>
+      );
+    }
+
+    return null;
+  }
+}
