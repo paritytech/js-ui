36aaf05a98d38bfcf0c4b362decb7828345b84f9
Jaco Greeff <jaco@parity.io>
2017-10-02 15:09:42 +0200

    Fix duplicate import

--- TxList/txList.js
+++ TxList/txList.js
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-import { observer } from 'mobx-react';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { observer } from 'mobx-react';
