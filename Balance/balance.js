4dd6dbe36778a72e45b30d8a953ba9b203aee634
Jaco Greeff <jaco@parity.io>
2017-10-17 15:59:17 +0200

    Add missing pick import

--- Balance/balance.js
+++ Balance/balance.js
@@ -15,6 +15,7 @@
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
 import BigNumber from 'bignumber.js';
+import { pick } from 'lodash';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
